c999a8e632a87437a57c55a800534ebcc06bb135
Patricia Garcia <pat@patriciagarcia.me>
2014-02-18 15:53:30 +0100

    LB-1457 Move request library to Backbone.sync method

--- gui-resources/scripts/js/core/backbone/backboneCustom.js
+++ gui-resources/scripts/js/core/backbone/backboneCustom.js
@@ -7,16 +7,48 @@ define(['backbone'], function(Backbone){
 
     Backbone.sync = function(method, model, options) {
 
-        if (method === 'read')  {
+        if (!options) { options = {}; }
+
+        if (method === 'read') {
+
+            // Set headers
             options.headers = {
+                // Date format that all browsers can parse
                 'X-Format-DateTime': 'yyyy-MM-ddTHH:mm:ss\'Z\''
             };
 
             if (model.xfilter) {
                 options.headers['X-Filter'] = model.xfilter;
             }
+
+            // Don't use default Backbone.sync for node.js
+            if (typeof process !== 'undefined' &&
+                    process.versions &&
+                    !!process.versions.node) {
+                return Backbone.nodeRequest(method, model, options);
+            }
         }
         return Backbone.oldSync(method, model, options);
+    };
+
+    Backbone.nodeRequest = function(method, model, options) {
+
+        var request = require('request');
+
+        options.url = model.url();
+        options.json = true;
+
+        // Use options.success and options.errors callbacks
+        // for compatibility with Backbone
+        request.get(options, function(error, response, data) {
+            if (!error && response.statusCode === 200) {
+                if (options.success) {
+                    return options.success(data);
+                }
+            } else if(options.error) {
+                return options.error(response);
+            }
+        });
     };
 
     return Backbone;
--- gui-resources/scripts/js/server.js
+++ gui-resources/scripts/js/server.js
@@ -1,10 +1,9 @@
 'use strict';
 
 var requirejs = require('requirejs'),
-    express = require('express'),
-    dust    = require('dustjs-linkedin'),
-    path      = require('path'),
-    request   = require('request');
+    express   = require('express'),
+    dust      = require('dustjs-linkedin'),
+    path      = require('path');
 
 var app = express();
 
@@ -43,14 +42,10 @@ requirejs(['appConfig'], function(appConfig){
 
     requirejs([
         'models/blog',
-        'collections/posts',
         'views/blog',
-        'views/post_baseTemplates',
-        'views/post_templates',
         'tmpl!themeBase/container',
-        'tmpl!theme/container',
         'tmpl!index'
-    ], function(Blog, Posts, BlogView) {
+    ], function(Blog, BlogView) {
 
         var objects = {
             blog: new Blog({ id: liveblog.id })
@@ -58,36 +53,24 @@ requirejs(['appConfig'], function(appConfig){
 
         /*jshint maxcomplexity:false */
         app.get('/', function(req, res) {
-            var options = {
-                url: objects.blog.get('publishedPosts').url(),
-                headers: {
-                    'X-Format-DateTime': 'yyyy-MM-ddTHH:mm:ss\'Z\'',
-                    'X-Filter': objects.blog.get('publishedPosts').xfilter
-                }
+            objects.blogView = new BlogView({ model: objects.blog });
+
+            var renderBlog = function(model, response, options) {
+                var html = objects.blogView.render().$el.html();
+
+                var ctx = {
+                    'content': function(chunk) {
+                        return chunk.map(function(chunk){
+                            chunk.end(html);
+                        });
+                    }
+                };
+                dust.render('index', ctx, function(err,out){
+                    res.send(out);
+                });
             };
-            request(options, function(error, response, data) {
-                if (!error && response.statusCode === 200) {
-                    objects.posts = new Posts(JSON.parse(data), { parse: true });
-                    objects.blog.set('publishedPosts', objects.posts);
 
-                    objects.blogView = new BlogView({ model: objects.blog });
-
-                    var html = objects.blogView.render().$el.html();
-
-                    var ctx = {
-                        'content': function(chunk) {
-                            return chunk.map(function(chunk){
-                                chunk.end(html);
-                            });
-                        }
-                    };
-                    dust.render('index', ctx, function(err,out){
-                        res.send(out);
-                    });
-                } else {
-                    console.log('Error in request: ' + error);
-                }
-            });
+            objects.blog.get('publishedPosts').fetch({ success: renderBlog });
         });
 
         var port = 3000;
