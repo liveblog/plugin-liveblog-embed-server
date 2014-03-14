5002e61a60503ea97350f1197f5e6e094969f64d
Patricia Garcia <pat@patriciagarcia.me>
2014-03-14 16:16:47 +0100

    LB-1511 Leave only server side code for first delivery

--- gui-resources/scripts/js/app.js
+++ gui-resources/scripts/js/app.js
@@ -2,7 +2,6 @@
 
 var requirejs = require('requirejs'),
     express   = require('express'),
-    dust      = require('dustjs-linkedin'),
     path      = require('path'),
     fs        = require('fs'),
     qs        = require('qs'),
@@ -131,18 +130,7 @@ requirejs([
 
             var renderBlog = function() {
                 var html = blogView.render().$el.html();
-                var ctx = {
-                    'liveblog': liveblog,
-                    'content': function(chunk) {
-                        return chunk.map(function(chunk){
-                            chunk.end(html);
-                        });
-                    }
-                };
-
-                dust.render('index', ctx, function(err,out){
-                    res.send(out);
-                });
+                res.send(html);
             };
 
             var fetchPosts = function(view) {
