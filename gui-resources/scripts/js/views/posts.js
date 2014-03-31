557502538238f6d4bc1777e1adc80bd6dea4a486
Patricia Garcia <pat@patriciagarcia.me>
2014-03-31 20:50:10 +0200

    LB-1383 Allow posts reordering

--- gui-resources/scripts/js/views/posts.js
+++ gui-resources/scripts/js/views/posts.js
@@ -42,6 +42,7 @@ define([
                 this.listenTo(this.collection, 'reset', this.render);
                 this.listenTo(this.collection, 'add', this.addPost);
                 this.listenTo(this.collection, 'remove', this.removePost);
+                this.listenTo(this.collection, 'change:Order', this.reorderPost);
             }
             this.collection.fetch({reset: true});
         },
@@ -63,9 +64,7 @@ define([
 
         addPost: function(post) {
             var postView = this.insertPostView(post);
-            // Reorder the views array before rendering the new view
-            this.views[this.rootSel] = _.sortBy(this.views[this.rootSel],
-                                                'order').reverse();
+            this.orderViews();
             postView.render();
         },
 
@@ -78,26 +77,63 @@ define([
             });
         },
 
-        // Override Backbone.LayoutManager to insert new posts in the right position
+        reorderPost: function(post, newOrder) {
+            // if more than 2 attributes has been updated (Order and CId)
+            // remove the view and render it again in the right position
+            if (_.size(post.changedAttributes()) > 2) {
+                this.removePost(post);
+                this.addPost(post);
+            } else {
+                // Update order attribute in view
+                this.views[this.rootSel][this._postViewIndex(post.id)].order =
+                                                            parseFloat(newOrder);
+
+                // Reorder views array
+                this.orderViews();
+
+                // Detach view and insert it in new position
+                var newIndex = this._postViewIndex(post.id),
+                    postEl = this.$(this.postRootSel(post.id)).detach();
+
+                this._insertPostViewAt(this.$el, postEl, newIndex);
+            }
+        },
+
         insert: function($root, $el) {
-            // Append the first posts (reset or backend posts rendering) to the view element
+            // on 'reset' append all elements to the root element
             if ($root.is(':empty')) {
                 $root.append($el);
-            // For posts added in sucessive updates (addPost)
             } else {
-                // Find the index where the new post view should be inserted
-                var orderedIds = this.views[this.rootSel].map(function(v) {
-                                        return v.model.id;
-                                    });
-                var i = _.indexOf(orderedIds, $el.attr(this.postRootDataAttr));
-                // If it's not the first place, find the previous post el
-                // by postId and add it after it
-                if (i > 0) {
-                    var prevId = this.views[this.rootSel][i - 1].model.id;
-                    $root.children(this.postRootSel(prevId)).after($el);
-                } else {
-                    $root.children(this.postRootSel()).first().before($el);
-                }
+            // on 'add' insert the new post in the right position
+                var i = this._postViewIndex($el.attr(this.postRootDataAttr));
+                this._insertPostViewAt($root, $el, i);
+            }
+        },
+
+        // Order the views array according to order param in views
+        orderViews: function() {
+            this.views[this.rootSel] = _.sortBy(this.views[this.rootSel],
+                                                'order').reverse();
+        },
+
+        // Returns the index of the post view on the views array or -1
+        // if the view is not in it
+        _postViewIndex: function(postId) {
+            var orderedIds = this.views[this.rootSel].map(function(v) {
+                                    return v.model.id;
+                                });
+            return _.indexOf(orderedIds, postId);
+        },
+
+        // Insert post element in the parent list element at a given index
+        _insertPostViewAt: function($parent, $el, i) {
+            // If i it's not 0, find the previous post el by postId and add
+            // it after it
+            if (i > 0) {
+                var prevId = this.views[this.rootSel][i - 1].model.id;
+                $parent.children(this.postRootSel(prevId)).after($el);
+            } else {
+                $parent.children(this.postRootSel()).first().before($el);
             }
         }
     });
