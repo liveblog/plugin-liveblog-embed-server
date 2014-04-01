bc708c04a048b711c9ad9c15803f21caadfca1bc
Patricia Garcia <pat@patriciagarcia.me>
2014-04-01 12:08:39 +0200

    LB-1519 After fetching render only new posts

--- gui-resources/scripts/js/views/posts.js
+++ gui-resources/scripts/js/views/posts.js
@@ -37,9 +37,10 @@ define([
 
         initialize: function() {
             utils.dispatcher.trigger('initialize.posts-view', this);
+            _.bindAll(this, 'insertPostView', 'orderViews', '_postViewIndex', '_insertPostViewAt');
             this.setTemplate('themeBase/posts-list');
             if (utils.isClient) {
-                this.listenTo(this.collection, 'reset', this.render);
+                this.listenTo(this.collection, 'reset', this.setViewOnReset);
                 this.listenTo(this.collection, 'add', this.addPost);
                 this.listenTo(this.collection, 'remove', this.removePost);
                 this.listenTo(this.collection, 'change:Order', this.reorderPost);
@@ -55,11 +56,50 @@ define([
             utils.dispatcher.trigger('after-render.posts-view', this);
         },
 
-        // Create a new post view and insert it at the end of the views array
-        insertPostView: function(post) {
-            var postView = new PostView({model: post});
-            this.insertView(this.rootSel, postView);
-            return postView;
+        setViewOnReset: function() {
+            // Set view $el element
+            this.$el = this.parentView().$(this.rootSel);
+
+            var postEls = this.$el.children(this.postRootSel());
+
+            // If there are no server side rendered posts, render whole view
+            if (postEls.length === 0) {
+                this.render();
+                return;
+            }
+
+            // Otherwise construct nested views from DOM
+            var self = this;
+            postEls.each(function(index, pEl) {
+                var postEl  = self.$(pEl),
+                    postId  = postEl.attr(self.postRootDataAttr),
+                    postCId = postEl.data('gimme-cid').toString(),
+                    post    = self.collection.get(postId);
+
+                // If the post is still in the collection and hasn't changed
+                // construct the postView and add it to nested views
+                if (post && postCId === post.get('CId')) {
+                    // TODO: We may need to set here postView.hasRendered = true or
+                    // fire an event here for the plugins
+                    var postView = self.insertPostView(post);
+                    postView.$el = postEl;
+
+                } else { // Else remove markup
+                    postEl.remove();
+                }
+            });
+
+            // Add new posts or posts that were removed because they had changed
+            this.collection.forEach(this.addPostIfMissing, this);
+
+            // TODO: We may need to fire an event here for the plugins
+        },
+
+        addPostIfMissing: function(post) {
+            var i = this._postViewIndex(post.id);
+            if (i === -1) {
+                this.addPost(post);
+            }
         },
 
         addPost: function(post) {
@@ -78,9 +118,9 @@ define([
         },
 
         reorderPost: function(post, newOrder) {
-            // if more than 2 attributes has been updated (Order and CId)
+            // if some attribute other than Order (and CId) has changed
             // remove the view and render it again in the right position
-            if (_.size(post.changedAttributes()) > 2) {
+            if (!this.onlyOrderHasChanged(post)) {
                 this.removePost(post);
                 this.addPost(post);
             } else {
@@ -110,15 +150,32 @@ define([
             }
         },
 
+        // Create a new post view and insert it at the end of the views array
+        insertPostView: function(post) {
+            var postView = new PostView({model: post});
+            this.insertView(this.rootSel, postView);
+            return postView;
+        },
+
         // Order the views array according to order param in views
         orderViews: function() {
             this.views[this.rootSel] = _.sortBy(this.views[this.rootSel],
                                                 'order').reverse();
         },
 
+        onlyOrderHasChanged: function(post) {
+            return (_.size(post.changedAttributes()) > 2 &&
+                post.hasChanged('CId') && post.hasChanged('Order'));
+        },
+
         // Returns the index of the post view on the views array or -1
         // if the view is not in it
         _postViewIndex: function(postId) {
+            // If there are no views yet on this.views[this.rootSel]
+            if (!this.views[this.rootSel]) {
+                return -1;
+            }
+
             var orderedIds = this.views[this.rootSel].map(function(v) {
                                     return v.model.id;
                                 });
@@ -137,5 +194,4 @@ define([
             }
         }
     });
-
 });
