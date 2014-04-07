8aeda0fbc555c28299a4f8085fcb3e0aa92905a4
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2014-04-07 16:49:58 +0300

    LB-1504 Added default logger if NODE_ENV was not found.

--- gui-resources/scripts/js/lib/logger.js
+++ gui-resources/scripts/js/lib/logger.js
@@ -5,15 +5,17 @@ var Log = require('log');
 
 var Logger = module.exports = function Logger(level, stream) {
 
-    // Set default level to info
+    // Set default level to info.
     level = level || 'info';
 
-    // For development stream to provided file
+    // For development stream to provided file.
     if (stream && process.env.NODE_ENV === 'development') {
         return new Log(level, stream);
-    // For 'production' & 'forever' stream to stdout
+    // For 'production' & 'forever' stream to stdout.
     } else if (process.env.NODE_ENV === 'production' ||
         process.env.NODE_ENV === 'forever') {
         return new Log(level);
     }
+    // Default behaviour is stdout.
+    return new Log(level);
 };
