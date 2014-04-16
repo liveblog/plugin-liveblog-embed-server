c3436033c64f61cc177821b5c33ab53f54ab1662
Patricia Garcia <pat@patriciagarcia.me>
2014-04-16 19:24:35 +0200

    LB-1588 Run server side tests with travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,6 +76,6 @@ module.exports = function(grunt) {
     grunt.registerTask('hint', ['jshint:all', 'jscs:all']);
     grunt.registerTask('test', ['karma:once', 'simplemocha:all']);
     grunt.registerTask('build', ['hint', 'less:all', 'requirejs']);
-    grunt.registerTask('ci:travis', ['hint', 'karma:ci']);
+    grunt.registerTask('ci:travis', ['hint', 'karma:ci', 'simplemocha:all']);
     grunt.registerTask('default', ['githooks', 'server']);
 };
