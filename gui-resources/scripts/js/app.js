922357a3761111f6e799f7629051a056a0a3a548
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2014-04-16 11:10:06 +0300

    LB-1662 Refactored requirejs clear cache method.
    Use a object instead of an Array.

--- gui-resources/scripts/js/app.js
+++ gui-resources/scripts/js/app.js
@@ -41,21 +41,21 @@ fs.exists(paths.logs, function(exists) {
     }
 });
 // Set a vector for caching on requirejs object.
-requirejs.cache = [];
+requirejs.cache = {};
 
 // This method is triggered by requirejs when a module is added,
 //   so we keep all of the module id into the requirejs cache object.
 requirejs.onResourceLoad = function (context, map, depArray) {
-    requirejs.cache.push(map.id);
+    requirejs.cache[map.id] = true;
 };
 
 // Check the require cache object for the matching pattern,
 //    and if modules with the pattern are found undefine them form requirejs.
 requirejs.clearCache = function(pattern) {
     var srex = new RegExp(pattern);
-    lodash.each(requirejs.cache, function(name, idx) {
-        if (srex.test(name)) {
-            requirejs.cache.splice(idx, 1);
+    lodash.each(requirejs.cache, function(value, name) {
+        if (value && srex.test(name)) {
+            requirejs.cache[name] = false;
             requirejs.undef(name);
         }
     });
