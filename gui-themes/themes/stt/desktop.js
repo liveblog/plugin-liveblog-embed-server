29a2d7bcd7cc53b55af8e339fa3d7b5181a89e45
vladnicoara <vlad.nicoara@sourcefabric.org>
2014-04-28 13:47:50 +0300

    LB-1505 Added wrappup plugin to stt theme

--- gui-themes/themes/stt/desktop.js
+++ gui-themes/themes/stt/desktop.js
@@ -1,13 +1,14 @@
 'use strict';
 
 define([
-	'css!theme/liveblog',
+    'css!theme/liveblog',
     'plugins/pagination',
     'plugins/button-pagination',
     'plugins/twitter-widgets',
     'plugins/post-hash',
     'plugins/permanent-link',
     'plugins/social-share',
+    'plugins/wrappup-toggle',
     'tmpl!theme/container',
     'tmpl!theme/item/base'
 ], function() {
