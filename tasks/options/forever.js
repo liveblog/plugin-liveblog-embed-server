6a37728ca1db23e7dcc6f54588fdb2b90d504c77
nistormihai <mihai.nistor@sourcefabric.org>
2014-05-05 17:01:19 +0300

    Update forever.js

--- tasks/options/forever.js
+++ tasks/options/forever.js
@@ -1,7 +1,7 @@
 module.exports = {
     liveblog: {
         options: {
-            index: '<%= paths.scripts %>/server.js',
+            index: '<%= paths.scripts %>server.js',
             logDir: '<%= paths.logs %>'
         }
     }
