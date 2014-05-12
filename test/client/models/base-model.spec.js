a8d76ba15bf3d5527f9e90d3c19469343a8465d2
vladnicoara <vlad.nicoara@sourcefabric.org>
2014-05-12 18:44:11 +0300

    LB-1592 Unit test base model

--- test/client/models/base-model.spec.js
+++ test/client/models/base-model.spec.js
@@ -1,17 +1,44 @@
 // TODO: Set a different jshint config for tests and remove the next lines
-/*global describe, it, before, beforeEach, after, afterEach, expect */
+/*global describe, it, before, beforeEach, after, afterEach, expect, sinon */
 /*jshint unused: false, -W030, -W024 */
 'use strict';
 
-define(['models/base-model'], function(BaseModel) {
+define(['jquery', 'models/base-model'], function($, Model) {
     describe('Base model', function() {
         describe('poller', function() {
-            console.log('base model');
-            it('starts the polling process', function() {
-                var model = new BaseModel(),
-                var options = {};
+            // Use Sinon to replace jQuery's ajax method
+            // with a spy.
+            beforeEach(function() {
+                sinon.stub($, 'ajax').yieldsTo('success', {
+                    Id: 1
+                });
+            });
+
+            // Restore jQuery's ajax method to its
+            // original state
+            afterEach(function() {
+                $.ajax.restore();
+            });
+            it('should make an ajax call to //localhost:9000', function(done) {
+                var model = new Model({Id: 1}),
+                    options = {
+                    url: '//localhost:9000',
+                    crossDomain: true
+                };
+
                 model.poller(options);
-                expect(model.Id).to.be('integer');
+                expect($.ajax.calledOnce).to.be.true;
+                expect($.ajax.getCall(0).args[0].url).to.equal('//localhost:9000');
+                expect($.ajax.getCall(0).args[0].crossDomain).to.be.true;
+                expect(model.get('Id')).equal(1);
+                done(); // let Mocha know we're done async testing
+            });
+
+        });
+        describe('properties', function() {
+            var model = new Model();
+            it('should have property idAttribute equall to "Id"', function() {
+                expect(model).to.have.property('idAttribute').to.equal('Id');
             });
         });
     });
