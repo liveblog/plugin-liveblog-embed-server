7b79ce521ff8c0e47c3cb6589ec6e9bd7e0e843d
actionless <actionless.loveless@gmail.com>
2014-06-19 17:20:32 +0200

    style(tests): protractor.conf.js

--- protractor-conf.js
+++ protractor-conf.js
@@ -2,36 +2,39 @@
 
 var ScreenShotReporter = require('protractor-screenshot-reporter');
 
-exports.config = {
-	baseUrl: 'http://nodejs-dev.sourcefabric.org/?liveblog[servers][rest]=master.lb-test.sourcefabric.org&liveblog[limit]=5&liveblog[theme]=default',
+//baseUrl: 'http://nodejs-dev.sourcefabric.org/?liveblog[servers][rest]=master.lb-test.sourcefabric.org&liveblog[limit]=5&liveblog[theme]=default',
 
-    specs: [
-        'spec/setup.js',
-        'spec/matchers.js',
-        'spec/**/*[Ss]pec.js'
-    ],
+exports.config = 
+{ baseUrl: 'http://nodejs-dev.sourcefabric.org'
+, params:
+  { baseBackendUrl: 'http://master.lb-test.sourcefabric.org/resources'
+  , baseUrl: 'http://nodejs-dev.sourcefabric.org'
+  }
+, specs:
+  [ 'spec/setup.js'
+  , 'spec/matchers.js'
+  , 'spec/**/*[Ss]pec.js'
+  ]
+, capabilities:
+  { browserName: 'chrome'
+  }
+, framework: 'jasmine'
+, jasmineNodeOpts:
+  { showColors: true
+  , isVerbose: false
+  , includeStackTrace: false
+  , defaultTimeoutInterval: 30000
+  }
 
-    capabilities: {
-        browserName: 'chrome'
-    },
-
-    framework: 'jasmine',
-
-    jasmineNodeOpts: {
-        showColors: true,
-        isVerbose: false,
-        includeStackTrace: false,
-        defaultTimeoutInterval: 30000
-    },
-
-    /* global jasmine */
-    onPrepare: function() {
-        jasmine.getEnv().addReporter(new ScreenShotReporter({
-            baseDirectory: './screenshots',
-            pathBuilder: function pathBuilder(spec, descriptions, results, capabilities) {
-                return results.passed() + '_' + descriptions.reverse().join('-');
-            },
-            takeScreenShotsOnlyForFailedSpecs: true
-        }));
+/* global jasmine */
+, onPrepare: function() {
+  jasmine.getEnv().addReporter(new ScreenShotReporter(
+  { baseDirectory: './screenshots'
+  , pathBuilder:
+    function pathBuilder(spec, descriptions, results, capabilities) {
+      return results.passed() + '_' + descriptions.reverse().join('-');
     }
+  , takeScreenShotsOnlyForFailedSpecs: true
+  }));
+  }
 };
