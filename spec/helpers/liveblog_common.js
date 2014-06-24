c548d9bad36a3e1423499ed1267c3a84a94f50e6
actionless <actionless.loveless@gmail.com>
2014-06-24 12:35:21 +0200

    style(tests): liveblog_common

--- spec/helpers/liveblog_common.js
+++ spec/helpers/liveblog_common.js
@@ -1,63 +1,61 @@
-var request = require('request');
-var constructUrl = require("./utils").constructUrl
+// make linter happy:
+var protractor = protractor;
+var browser = browser;
 
+var request = require('request');
+var constructUrl = require("./utils").constructUrl;
 
 exports.getBackendUrl = function getBackendUrl(uri)
 // construct app url from uri
 {
-; return constructUrl
-( protractor.getInstance().params.baseBackendUrl, uri )
-}
+  return constructUrl(
+    protractor.getInstance().params.baseBackendUrl, uri
+  );
+};
 
 exports.getUrl = function getUrl(uri)
 // construct backend url from uri
 {
-; return constructUrl
-( protractor.getInstance().params.baseUrl, uri )
-}
+  return constructUrl(
+    protractor.getInstance().params.baseUrl, uri
+  );
+};
 
 exports.goto = function gotoUri(uri)
 // go to app's uri
 {
-; return function()
-  {
-  ; var url = exports.getUrl(uri)
-  ; browser.driver.get(url)
-  ; console.log(url)
-  }
-}
+  return function() {
+    var url = exports.getUrl(uri);
+    browser.driver.get(url);
+    console.log(url);
+  };
+};
 
-exports.backendRequest = function(params, callback, token)
-{
-; if (token)
-  { 
-  ; if (!params.headers)
-    {
-    ; params.headers = {}
+exports.backendRequest = function(params, callback, token) {
+  if (token) {
+    if (!params.headers) {
+      params.headers = {};
     }
-  ; params.headers.Authorization = token
+    params.headers.Authorization = token;
   }
-; request
-( params
-, function(error, response, body)
-  {
-  ; if (error) {throw new Error(error)}
-  ; if ( (response.statusCode != 200) && (response.statusCode != 201) )
-    {
-    ; throw new Error("Status code: " + response.statusCode)
+  request(
+    params,
+    function(error, response, body) {
+      if (error) {
+        throw new Error(error);
+      }
+      if ((response.statusCode !== 200) && (response.statusCode !== 201)) {
+        throw new Error("Status code: " + response.statusCode);
+      }
+      callback(error, response, body);
     }
-  ; callback(error, response, body)
-  }
-)
-}
+  );
+};
 
-
-exports.backendRequestAuth = function(params, callback)
-{
-; var token = protractor.getInstance().params.token
-; if (! token)
-  {
-  ; throw new Error('No auth token')
+exports.backendRequestAuth = function(params, callback) {
+  var token = protractor.getInstance().params.token;
+  if (!token) {
+    throw new Error('No auth token');
   }
-; exports.backendRequest(params, callback, token)
+  exports.backendRequest(params, callback, token);
 };
