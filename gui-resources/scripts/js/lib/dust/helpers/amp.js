daaa741423a00388cf619b130a52c81bd6b38bdb
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2017-03-23 17:55:30 +0200

    fix: bypass `.ico`files.

--- gui-resources/scripts/js/lib/dust/helpers/amp.js
+++ gui-resources/scripts/js/lib/dust/helpers/amp.js
@@ -2,19 +2,33 @@
 
 define(['dust/core', 'lib/utils'], function(dust, utils) {
 
+    var bypassExtensions = ['.ico'];
     dust.filters.amp = function(string) {
         if (utils.isServer) {
-            var imageSize = require('image-size'),
-            request = require('request');
 
             string = string.replace(/<img([^>]*)>/g, function(all, attr) {
-
+                var bypass = false;
                 attr = attr.replace(/src\w*=\w*("|')([^\"\']+)("|')/g, function(at, qt, src) {
-                    var chunks = [], size = {height: -1, width: -1}, found = false;
+                    var chunks = [],
+                        size = {height: -1, width: -1},
+                        found = false;
+
+                    for(var i=0, count = bypassExtensions.length; i < count; i++) {
+                        if(src.substr(src.length-4) === bypassExtensions[i]) {
+                            bypass = true;
+                        }
+                    }
+                    if(bypass) {
+                        return at;
+                    }
                     try {
                         if (src.substr(0, 4).toLowerCase() !== 'http') {
                             src = 'http:' + src;
                         }
+
+                        var imageSize = require('image-size'),
+                            request = require('request');
+
                         var r = request.get({uri: src})
                             .on('data', function (chunk) {
                                 chunks.push(chunk);
@@ -37,6 +51,9 @@ define(['dust/core', 'lib/utils'], function(dust, utils) {
                     }
                     return 'src="' + src + '" height="' + size.height + '" width="' + size.width + '"';
                 });
+                if(bypass) {
+                    return all;
+                }
                 return '<amp-img layout="responsive"' + attr + '/>';
             });
 
